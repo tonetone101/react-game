dd203134f32218012ed0f78ba3564602ad691e8f
Antkeo1 <sleav143@gmail.com>
2019-06-21 16:00:13 -0400

    canvas created

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
+import Game from './containers/Game';
+import reducer from './reducers';
+import registerServiceWorker from './registerServiceWorker';
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+    reducer, /* preloadedState, */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider store={store}>
+        <Game />
+    </Provider>,
+    document.getElementById('root'),
 );
+registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  message: `Its's easy to integrate React and Redux`,
+};
+
+function reducer(state = initialState) {
+  return state;
+}
+
+export default reducer;

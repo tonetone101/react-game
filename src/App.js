eb11996e7a18a36687123191db9c3100dc0fb765
Antkeo1 <sleav143@gmail.com>
2019-06-21 16:22:09 -0400

    used rect to create sky

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Canvas from './components/Canvas'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>{this.props.message}</h1>
+        <Canvas />
       </div>
     );
   }
-}
-
-App.propTypes = {
-  message: PropTypes.string.isRequired,
 }
 
 export default App;
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 
 const Canvas = () => {
-  const style = {
-    border: '1px solid black',
-  };
-
+  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
   return (
     <svg
-      id='aliens-go-home-canvas'
-      preserveAspectRatio='xMaxYMax none'
-      style={style}
+      id="aliens-go-home-canvas"
+      preserveAspectRatio="xMaxYMax"
+      viewBox={viewBox}
     >
       <circle cx={0} cy={0} r={50} />
     </svg>
-  )
+  );
 };
 
 export default Canvas;
